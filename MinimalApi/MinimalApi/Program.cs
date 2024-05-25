aedc6f8367f5f478bab4da34500fb537e31b5f72
maxvone <maximvone@gmail.com>
2024-05-25 13:10:35 +0200

    [ADD] add swashbuckle, swagger

--- MinimalApi/MinimalApi/Program.cs
+++ MinimalApi/MinimalApi/Program.cs
@@ -2,9 +2,20 @@ using MinimalApi.Routers;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//Packages services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Project services
 builder.Services.AddScoped<IQuoteRouter, QuoteRouter>();
 
 var app = builder.Build();
+
+if(app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
 
 using (var scope = app.Services.CreateScope())
 {
